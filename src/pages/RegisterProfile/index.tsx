1ebb961d53ad04b6d2ad72154760d998d4ca0e7f
Luciana Nascimento <luciananssantana@gmail.com>
2022-10-20 13:58:16 -0300

    forms pagina profile

--- src/pages/RegisterProfile/index.tsx
+++ src/pages/RegisterProfile/index.tsx
@@ -1,31 +1,121 @@
+import React, {FormEvent, useEffect, useState} from "react";
 import "./styles.css";
 import { Navbar } from '../../components/Navbar/index';
 import Select from 'react-select';
 
+import api from '../../services/api';
+
+
 export function RegisterProfile() {
 
+    const [nome, setNome] = useState('');
+    const [ptaLeite, setPtaLeite] = useState(0.0);
+    const [operadorPtaLeite, setOperadorPtaLeite] = useState('')
+    const [ptaTipo, setPtaTipo] = useState(0.0);
+    const [operadorPtaTipo, setOperadorPtaTipo] = useState('')
+    const [indUbere, setIndUbere] = useState(0.0);
+    const [operadorIndUbere, setOperadorIndUbere] = useState('')
+    const [indConf, setIndConf] = useState(0.0);
+    const [operadorIndConf, setOperadorIndConf] = useState('')
+    const [dpr, setDpr] = useState(0.0);
+    const [operadorDpr, setOperadorDpr] = useState('')
+    const [vidaProd, setVidaProd] = useState(0.0);
+    const [operadorVidaProd, setOperadorVidaProd] = useState('')
+    const [indCompPerna, setIndCompPerna] = useState(0.0);
+    const [operadorIndCompPerna, setOperadorIndCompPerna] = useState('')
+    const [indBetaCaseina, setIndBetaCaseina] = useState(false);
+    const [operadorIndBetaCaseina, setOperadorIndBetaCaseina] = useState('')
+    const [consanguinidade, setConsanguinidade] = useState(0.0);
+    const [operadorConsanguinidade, setOperadorConsanguinidade] = useState('')
+    
     const optionsSelect = [
         { value: '>', label: '>' },
         { value: '>=', label: '>=' },
         { value: '<', label: '<' },
         { value: '<=', label: '<=' },
         { value: '=', label: '=' }
-      ]
+    ]
 
       
+    const onSubmit =  (event: FormEvent) => {
+        event.preventDefault();
+
+        api.post("/selection-profile", {
+            name: nome,
+            forms: [
+                {
+                    profile_items: {
+                        productive_life: vidaProd
+                    },
+                    operator: operadorVidaProd
+                },
+                {
+                    profile_items: {
+                        conformation_index: indConf
+                    },
+                    operator: operadorIndConf
+                },
+                {
+                    profile_items: {
+                        consanguinity: consanguinidade
+                    },
+                    operator: operadorConsanguinidade
+                },
+                {
+                    profile_items: {
+                        ptat: ptaTipo
+                    },
+                    operator: operadorPtaTipo
+                },
+                {
+                    profile_items: {
+                        legs_composition: indCompPerna
+                    },
+                    operator: operadorIndCompPerna
+                },
+                {
+                    profile_items: {
+                        dpr: dpr
+                    },
+                    operator: operadorDpr
+                },
+                {
+                    profile_items: {
+                        beta_casein: indBetaCaseina ? 1 : 0
+                    },
+                    operator: '='
+                },
+                {
+                    profile_items: {
+                        udder_index: indUbere
+                    },
+                    operator: operadorIndUbere
+                },
+                {
+                    profile_items: {
+                        ptal: ptaLeite
+                    },
+                    operator: operadorPtaLeite
+                },
+            ]
+        })
+
+    }
+
     return (
         <div className="App">
             <Navbar/>
             <div className="container">
                 <div className="profile-registration">
                 <h1 className="grid-title">Informações do Perfil de Seleção</h1>
-                <form>
+                <form onSubmit={onSubmit}>
                     <label className="form-label">Nome</label> 
                     <div style={{display: "flex"}}>  
                         <input
                         type="text"
                         id="nome"
                         name="nome"
+                        onChange={(event) => setNome(event.target.value)}
                         />
                     </div>
                     <br />
@@ -36,9 +126,10 @@ export function RegisterProfile() {
                             <Select options={optionsSelect}/>
                         </div>
                         <input
-                        type="text"
+                        type="number"
                         id="pta_leite"
                         name="pta_leite"
+                        onChange={(event) => setPtaLeite(Number(event.target.value))}
                         />
                     </div>
                     <br />
@@ -47,9 +138,10 @@ export function RegisterProfile() {
                     <div style={{display: "flex"}}>
                         <Select options={optionsSelect} />
                         <input
-                        type="text"
+                        type="number"
                         id="pta_tipo"
                         name="pta_tipo"
+                        onChange={(event) => setPtaTipo(Number(event.target.value))}
                         />
                     </div>
                     <br />
@@ -58,9 +150,10 @@ export function RegisterProfile() {
                     <div style={{display: "flex"}}>    
                         <Select options={optionsSelect} />
                         <input
-                        type="text"
+                        type="number"
                         id="ind_ubere"
                         name="ind_ubere"
+                        onChange={(event) => setIndUbere(Number(event.target.value))}
                         />
                     </div>
                     <br />
@@ -69,9 +162,10 @@ export function RegisterProfile() {
                     <div style={{display: "flex"}}>   
                         <Select options={optionsSelect} /> 
                         <input
-                        type="text"
+                        type="number"
                         id="ind_conf"
                         name="ind_conf"
+                        onChange={(event) => setIndConf(Number(event.target.value))}
                         />
                     </div>
                     <br />
@@ -80,9 +174,10 @@ export function RegisterProfile() {
                     <div style={{display: "flex"}}>   
                         <Select options={optionsSelect} />
                         <input
-                        type="text"
+                        type="number"
                         id="dpr"
                         name="dpr"
+                        onChange={(event) => setDpr(Number(event.target.value))}
                         />
                     </div>
                     <br />
@@ -91,9 +186,10 @@ export function RegisterProfile() {
                     <div style={{display: "flex"}}> 
                         <Select options={optionsSelect} />   
                         <input
-                        type="text"
+                        type="number"
                         id="vida_prod"
                         name="vida_prod"
+                        onChange={(event) => setVidaProd(Number(event.target.value))}
                         />
                     </div>
                     <br />
@@ -102,20 +198,22 @@ export function RegisterProfile() {
                     <div style={{display: "flex"}}>    
                         <Select options={optionsSelect} />
                         <input
-                        type="text"
+                        type="number"
                         id="ind_composto"
                         name="ind_composto"
+                        onChange={(event) => setIndCompPerna(Number(event.target.value))}
                         />
                     </div>
                     <br />
                     <br />
-                    <label className="form-label">Indicador de Beta Caseína</label>
+                    <label className="form-label">Indicador de Beta Caseína (A2A2)</label>
                     <div style={{display: "flex"}}>
-                        <Select options={optionsSelect} />
                         <input
+                        className="checkBoxBeta"
                         type="checkbox"
                         id="ind_beta"
                         name="ind_beta"
+                        onChange={(event) => setIndBetaCaseina(event.target.checked)}
                         />
                     </div>
                     <br />
@@ -124,13 +222,19 @@ export function RegisterProfile() {
                     <div style={{display: "flex"}}>    
                         <Select options={optionsSelect} />
                         <input
-                        type="text"
+                        type="number"
                         id="consang"
                         name="consang"
+                        onChange={(event) => setConsanguinidade(Number(event.target.value))}
                         />
                     <label className="form-label">%</label> 
                     </div>
-                     
+                    <button
+                        type='submit'
+                        className="submitButton"
+                    >
+                        Salvar
+                    </button>
                 </form>
                 </div>
             </div>
