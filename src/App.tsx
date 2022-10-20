3ee31149aba7b925ad6dc83ffb2e3569fbaa951c
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2022-10-20 01:50:35 -0300

    tela de pesquisa/resultado

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import "./nav.css";
+import "./results.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -22,6 +23,138 @@ function App() {
           </a>
         </div>
       </nav>
+
+      <div className="container">
+        <div className="cow-registration">
+          <h1 className="grid-title">Informações da vaca</h1>
+          <form>
+            <label className="form-label">Nome do Pai</label>
+            <br />
+            <input
+              type="text"
+              id="first_level_parent"
+              name="first_level_parent"
+            />
+            <br />
+            <label className="form-label">Nome do Avô</label>
+            <br />
+            <input
+              type="text"
+              id="second_level_parent"
+              name="second_level_parent"
+            />
+          </form>
+        </div>
+        <div className="results">
+          <h1 className="grid-title">Resultados</h1>
+          <div className="table-wrapper">
+            <table className="results-table">
+              <thead>
+                <tr>
+                  <th>Nome</th>
+                  <th>Nome do Pai</th>
+                  <th>Nome do Avô</th>
+                  <th>Nome do Bisavô</th>
+                  <th>PTA Leite</th>
+                  <th>PTA Tipo</th>
+                  <th>Índice de Úbere</th>
+                  <th>Índice de Conformação</th>
+                  <th>DPR</th>
+                  <th>Vida Produtiva</th>
+                  <th>Índice de Composto de Pernas</th>
+                  <th>Índice de Beta Caseína</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Dom</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                </tr>
+                <tr className="active-row">
+                  <td>Melissa</td>
+                  <td>5150</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                </tr>
+                <tr>
+                  <td>Dom</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                </tr>
+                <tr>
+                  <td>Melissa</td>
+                  <td>5150</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                </tr>
+                <tr>
+                  <td>Dom</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                </tr>
+                <tr>
+                  <td>Melissa</td>
+                  <td>5150</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                  <td>6000</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
 
       {/* <div>
         <a href="https://vitejs.dev" target="_blank">
